6f7c2e6bfab42a1f92bb3d451e8945e13fc603e8
Whaleep <billy_1817@yahoo.com.tw>
2021-06-20 03:58:41 +0800

    Initialize Project

---
+++ README.md
@@ -0,0 +1,32 @@
+# 餐廳清單 Restaurant List
+一個用 Express 所建立簡單的餐廳清單網路應用程式。
+
+## 特色 Features
+- 在首頁瀏覽所有餐廳與它們的簡單資料，包含餐廳照片、名稱、分類、評分
+- 可以點進去看餐廳的詳細資訊，包含類別、地址、電話、描述、圖片
+- 可以根據餐廳名稱或分類來找到特定的餐廳
+
+## 環境建置 Environment Setup
+
+1. nvm & Node.js
+2. nodemon
+
+## 安裝 Installing
+
+1. 在終端機中移動到要安裝的目錄位置，並 Clone 此專案至本機電腦
+```
+git clone https://github.com/Whaleep/restaurantList.git
+```
+2. 安裝相關套件
+```
+npm install
+```
+3. 啟動專案
+```
+npm run dev
+```
+4. 出現以下訊息後，即可在 http://localhost:3000 開始使用
+```
+Express is listening on localhost:3000
+```
+
---
+++ app.js
@@ -0,0 +1,48 @@
+// require package used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require handlebars in the project
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+// experess template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('static'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+// querystring
+app.get('/search', (req, res) => {
+  const restaurants = restaurantList.results.filter(function (restaurant) {
+    const keyword = req.query.keyword.toLowerCase()
+    return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
+  })
+
+  if (restaurants.length > 0) {
+    res.render('index', { restaurants: restaurants, keyword: req.query.keyword })
+  }
+  else {
+    res.render('index', { keyword: req.query.keyword, no_result: '<h3>搜尋沒有結果</h3>' })
+  }
+
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(function (restaurant) {
+    return restaurant.id.toString() === req.params.restaurant_id
+  })
+  res.render('show', { restaurant: restaurant })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
