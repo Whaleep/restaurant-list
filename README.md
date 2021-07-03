b79ab01c179337ba91046d6e8316d8ab9377bf0d
Whaleep <billy_1817@yahoo.com.tw>
2021-07-03 23:20:17 +0800

    feat: add restaurant sort function

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## 特色 Features
 - 在首頁瀏覽所有餐廳與它們的簡單資料，包含餐廳照片、名稱、分類、評分
 - 可以點進去看餐廳的詳細資訊，包含類別、地址、電話、描述、圖片
-- 可以根據餐廳名稱或分類來找到特定的餐廳
+- 可以根據餐廳名稱、分類或排序來找到特定的餐廳
 - 可以新增、修改、刪除餐廳的資訊
 
 ## 環境建置 Environment Setup
@@ -17,11 +17,11 @@
 
 1. 在終端機輸入指令 Clone 此專案至本機電腦
 ```
-git clone https://github.com/Whaleep/restaurantList-v2.git
+git clone https://github.com/Whaleep/restaurantList-v3.git
 ```
 2. 進入專案目錄
 ```
-cd restaurantList-v2
+cd restaurantList-v3
 ```
 3. 安裝相關套件
 ```
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,19 +7,28 @@ router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .sort({ _id: 'asc' })
-    .then(restaurants => res.render('index', { restaurants }))
+    .then(restaurants => res.render('index', { restaurants, sort: "_id"}))
     .catch(error => console.log(error))
 })
 
 router.get('/search', (req, res) => {
   const keyword = new RegExp(req.query.keyword.trim(), 'i')
+  const sortList = {
+    "_id": { _id: 'asc' },
+    "nameAsc": { name_en: 'asc' },
+    "nameDesc": { name_en: 'desc' },
+    "category": { category: 'asc' },
+    "location": { location: 'asc' },
+    "rating": { rating: 'desc' }
+  }
   Restaurant.find({ $or: [{ name: keyword }, { category: keyword }] })
     .lean()
+    .sort(sortList[req.query.sort])
     .then(function (restaurants) {
       if (restaurants.length > 0) {
-        res.render('index', { restaurants: restaurants, keyword: req.query.keyword.trim() })
+        res.render('index', { restaurants: restaurants, query: req.query })
       } else {
-        res.render('index', { keyword: req.query.keyword, no_result: '<h3>搜尋沒有結果</h3>' })
+        res.render('index', { no_result: '<h3>搜尋沒有結果</h3>', query: req.query })
       }
     })
     .catch(error => console.log(error))
